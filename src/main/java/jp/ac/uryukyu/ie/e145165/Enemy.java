325e394ba491b41d9cc413894b7bf629aec3344f
Masaaki Matsunaga <castle2164m@gmail.com>
2017-01-19 18:32:05 +0900

    programing2_report6 ステップ１：コードの準備。

---
+++ src/main/java/jp/ac/uryukyu/ie/e145165/Enemy.java
@@ -0,0 +1,13 @@
+package jp.ac.uryukyu.ie.e145165;
+
+/**
+ * Created by masaaki on 2017/01/19.
+ */
+
+    public class Enemy extends LivingThing {
+        public Enemy(String name, int maximumHP, int attack) {
+            super(name, maximumHP, attack);
+
+        }
+    }
+
